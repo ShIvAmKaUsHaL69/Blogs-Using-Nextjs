ee414d174d0d15dc92959310e327c69fc86d33c4
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-02 20:22:53 +0530

    made the hero section of app and search functionality for searching blogs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
-  console.log('hello')
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string}>
+} ) {
+
+  const query = (await searchParams).query;
   return (
   <>
-  <h1 className="text-2xl">Home</h1>
+    <section className="pink_container">
+      <h1 className="heading">Share Your Voice, <br /> Inspire the World</h1>
+      <p className="sub-heading !max-w-3xl">A place to write, connect, and be heard</p>
+      <SearchForm query={query}/>
+    </section>
+    
   </>
   );
 }
