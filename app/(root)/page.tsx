7f0890103545c82388e98b73ddf75dc74d47581d
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-03 20:59:35 +0530

    implemented card for each blog to show , implemented query functionality on it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+
+import BlogCard from "@/components/BlogCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: {
@@ -5,12 +7,34 @@ export default async function Home({ searchParams }: {
 } ) {
 
   const query = (await searchParams).query;
+
+  const post = [{ _createdAt: new Date(),
+                  views: 55,
+                  author: { _id: 1, name: 'shivam' },
+                  _id: 1,
+                  description: 'this is a description.',
+                  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Minecraft-creeper-face.jpg/800px-Minecraft-creeper-face.jpg',
+                  category: 'Robots',
+                  title: 'We Robots',
+               },
+              ];
   return (
   <>
     <section className="pink_container">
       <h1 className="heading">Share Your Voice, <br /> Inspire the World</h1>
       <p className="sub-heading !max-w-3xl">A place to write, connect, and be heard</p>
       <SearchForm query={query}/>
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"` : 'All Blogs'} 
+      </p>
+      <ul className="mt-7 card_grid">
+        {post?.length > 0 ?(
+          post.map((BlogCardType,i) => (<BlogCard key={BlogCardType._id} post={BlogCardType}/>))
+        ) : (<p className="no-results">No Blogs found</p>)}
+      </ul>
+
     </section>
     
   </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,43 @@
-import { auth , signOut , signIn } from '@/auth'
+import { auth, signOut, signIn } from '@/auth'
 import Link from 'next/link'
 
 
 
 export default async function Navbar() {
     const session = await auth()
-  return (
-    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-        <nav className='flex justify-between items-center'>
-            <Link href='/'>
-            <img src='/logo.png' alt='logo' width={60} height={30} className='rounded-full'/>
-            </Link>
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href='/'>
+                    <img src='/logo.png' alt='logo' width={60} height={30} className='rounded-full' />
+                </Link>
 
-            <div className='flex items-center gap-5 text-black'>
-                {session && session?.user ? (
-                    <>
-                        <Link href='/blogs/create'>
-                            <span>Create</span>
-                        </Link>
-                        <form action={ async () => {
-                            'use server';
-                            await signOut({redirectTo: '/'})
+                <div className='flex items-center gap-5 text-black'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href='/blogs/create'>
+                                <span>Create</span>
+                            </Link>
+                            <form action={async () => {
+                                'use server';
+                                await signOut({ redirectTo: '/' })
+                            }}>
+                                <button type='submit'>signout</button>
+                            </form>
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form action={async () => {
+                            "use server";
+                            await signIn('github')
                         }}>
-                            <button type='submit'>signout</button>
+                            <button type='submit'>Login</button>
                         </form>
-                        <Link href={`/user/${session?.id}`}>
-                            <span>{session?.user?.name}</span>
-                        </Link>
-                    </>
-                ) : (
-                    <form action={ async () => {
-                        "use server";
-                        await signIn('github')}}>
-                        <button type='submit'>Login</button>
-                    </form>
-                )}
-            </div>
-        </nav>
-    </header>
-  )
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
 }
